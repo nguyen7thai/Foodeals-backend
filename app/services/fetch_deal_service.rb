eb20f0d7845e329bb7b35a53c3983ab619686f2f
Thai Nguyen <nguyen7thai@gmail.com>
2016-03-09 11:42:19 +0700

    Fetch deals for Ha Noi City

--- app/services/fetch_deal_service.rb
+++ app/services/fetch_deal_service.rb
@@ -1,17 +1,21 @@
 require 'open-uri'
 
 class FetchDealService
-  def self.exec
-    page_url = "http://www.nhommua.com/ho-chi-minh/an-uong"
+  CITY_CODE = [
+    HO_CHI_MINH = "ho-chi-minh",
+    HA_NOI = "ha-noi"
+  ]
+
+  def self.fetch_for_city(city_code)
+    page_url = "http://www.nhommua.com/#{city_code}/an-uong"
     root_url = "http://www.nhommua.com"
-    cookie_location = "NewCityId=1"
-    page = Nokogiri::HTML(open(page_url, "Cookie" => cookie_location))
+    page = Nokogiri::HTML(open(page_url))
     deal_items = []
     page.css('#cate_list > li').each do |deal|
       title = deal.css('.list_name').text
       link = deal.css('.img > a').attribute('href').value
       abs_link = "#{root_url}#{link}"
-      deal_page = Nokogiri::HTML(open(abs_link, "Cookie" => cookie_location))
+      deal_page = Nokogiri::HTML(open(abs_link))
       locations = deal_page.css('.link_map')
       evoucher = deal_page.xpath('//span[contains(text(), "(Email/SMS)")]')
       coordinate = nil
@@ -25,6 +29,13 @@ class FetchDealService
         coordinate = coordinate.split(',')
         deal_items << DealItem.new(title, abs_link, nil, coordinate)
       end
+    end
+    deal_items
+  end
+
+  def self.exec
+    deal_items = CITY_CODE.inject([]) do |result, code|
+      result += self.fetch_for_city(code)
     end
     { deal_items: deal_items }
   end
